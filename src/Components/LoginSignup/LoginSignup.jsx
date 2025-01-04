27101cfdb431f9a85cd795a4f84f011abd287f57
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-04 20:57:44 +0200

    Added: input fields, icons, basic texts

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,11 +1,39 @@
-import './LoginSignup.css';
+import "./LoginSignup.css";
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
+import user_icon from "../Assets/person.png";
+import email_icon from "../Assets/email.png";
+import password_icon from "../Assets/password.png";
 
 const LoginSignup = () => {
-    return (  );
-}
- 
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" name="" id="" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" name="" id="" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" name="" id="" />
+        </div>
+      </div>
+      <div className="forgot-password">
+        Forgot Password? <span> Click here!</span>
+      </div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Login</div>
+      </div>
+    </div>
+  );
+};
+
 export default LoginSignup;
