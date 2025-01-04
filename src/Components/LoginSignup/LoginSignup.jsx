efbde6f4e67b2d27617b9a380973b9adee096ba0
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-04 20:49:29 +0200

    cleaning the project and creating the structure

---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,11 @@
+import './LoginSignup.css';
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+const LoginSignup = () => {
+    return (  );
+}
+ 
+export default LoginSignup;
