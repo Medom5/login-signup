5c8f2ec64da1c37f7ca2c485d068ecde6cb52226
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-04 21:42:32 +0200

    Add: logic with useState

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -3,12 +3,16 @@ import "./LoginSignup.css";
 import user_icon from "../Assets/person.png";
 import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";
+import { useState } from "react";
 
 const LoginSignup = () => {
+
+    const [action, setAction] = useState("Sign Up");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
@@ -29,8 +33,8 @@ const LoginSignup = () => {
         Forgot Password? <span> Click here!</span>
       </div>
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action==="Login"? "submit gray": "submit"}>Sign Up</div>
+        <div className={action==="Sign Up"? "submit gray":"submit"}>Login</div>
       </div>
     </div>
   );
